8998e586b3b3508cf8aec6a53ae3d9524f6fe84d
katreddyvenkateswaraReddy <63310358+katreddyvenkateswaraReddy@users.noreply.github.com>
2023-11-09 11:34:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your JS code here. If required.
+// your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+    const body = document.body;
+    body.textContent = "DOM load success";
+});
+
